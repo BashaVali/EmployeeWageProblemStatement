3e0791655e50b137206d649bdf42739b9ab4f3e8
BashaVali <bashavali222@gmail.com>
2022-12-28 02:08:17 +0530

    EmployeeWage Problem

---
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationProblem
+{
+    public class EmployeeWageProblem
+    {
+        // class variable // UC-1
+        public const int Per_hour = 20;
+        public const int Full_Day_Hour = 8;
+        public const int Part_Time_Hour = 4;
+        public const int Wage_per_Hour = 20;
+        Random random = new Random();
+        public void EmployeeAttendence()
+        {
+            Random random = new Random();
+            int num = random.Next(2);
+
+            if (num == 0)
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            else
+            {
+                Console.WriteLine("Employee is present");
+            }
+        }
+    }
+}
---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the EmployeeWageComputationProblem");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("****** Employee Wage Problem ******");
+
+                Console.WriteLine(" Select the option ");
+
+                Console.WriteLine("1.Attendance" + "\n" +
+                                  "2.Exit" + "\n");
+                int check = Convert.ToInt32(Console.ReadLine());
+                switch (check)
+                {
+                    case 1:
+                        EmployeeWageProblem employeeWageProblem = new EmployeeWageProblem();
+                        employeeWageProblem.EmployeeAttendence();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
